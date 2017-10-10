733ab1b6a5170a073c6a94c1d9daa50c7f559355
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-10-09 19:57:50 -0500

    ssss

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
 module BoatRentalWebsite
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
