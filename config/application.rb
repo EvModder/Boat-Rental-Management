124c60c53dbda1ae544d712f85b1aef11626df46
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-10-09 19:59:26 -0500

    joj

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 
 module BoatRentalWebsite
   class Application < Rails::Application
@@ -24,5 +23,6 @@ module BoatRentalWebsite
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
