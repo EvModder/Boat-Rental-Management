1fd98d376fc0601c4f5b5da8b5bd875645122fab
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-10-30 01:18:33 -0500

    better renting page with rent-now button(not working yet)

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
 
   resources :captains
   mount Commontator::Engine => '/commontator'
+
+  #boat renting
+  get "boats/rent" => "boats#rent"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
