bc4e97c694a648587b7f02b15975bd32a1766183
Nathan Leake <leakenate@gmail.com>
2017-11-07 01:58:05 +0000

    email confirmation part1

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+#config.action_mailer.default_url_options = { host: 'localhost:3000' }
+#ActionMailer::Base.smtp_settings = {
+#   :tls => true,
+#   :address => "smtp.gmail.com",
+#   :port => "587",
+#   :domain => "gmail.com",
+#   :authentication => :login,
+#   :user_name => "leakenate",
+#   :password => "[password]"
+#}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030032213) do
+ActiveRecord::Schema.define(version: 20171107000928) do
 
   create_table "boat_attachments", force: :cascade do |t|
     t.integer  "boat_id"
@@ -121,8 +121,13 @@ ActiveRecord::Schema.define(version: 20171030032213) do
     t.string   "city"
     t.string   "state"
     t.string   "zip_code"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
