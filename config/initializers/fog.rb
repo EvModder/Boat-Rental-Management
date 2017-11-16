e28b996db584b8df467f4b2fd43ba8807b4aba05
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-11-16 15:06:19 -0600

    s3 v1

---
+++ config/initializers/fog.rb
@@ -0,0 +1,15 @@
+CarrierWave.configure do |config|
+  #config.fog_provider = 'fog/aws'                        # required
+  #config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     'AKIAILG26VPIWQPYEXMQ',                        # required
+      aws_secret_access_key: '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',                        # required
+      region:                'us-east-2',                  # optional, defaults to 'us-east-1'
+      #host:                  's3.example.com',             # optional, defaults to nil
+      #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'asset'                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
