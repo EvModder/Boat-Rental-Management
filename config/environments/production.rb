302a7a1941f95a006c6bcdd007547fabdada1b5d
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-11-26 11:36:44 -0600

    add smtp_settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => "587",
+      :domain => "gmail.com",
+      :authentication => :login,
+      :user_name => "development.nate",
+      :password => "Randomizing0"
+  }
 end
