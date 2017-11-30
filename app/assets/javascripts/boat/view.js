9c1dc1d010ca2a7e91dd2c8d32a0f6ceac078192
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-11-30 09:48:32 -0600

    1.chosen date is displayed after clicking rent button. 2.dropdown menu for users

--- app/assets/javascripts/boat/view.js
+++ app/assets/javascripts/boat/view.js
@@ -1,8 +1,10 @@
 $( document ).on('turbolinks:load',function()
 {
     var received_dates=$('#available-date').val();
+    var chosen_dates=$('#chosen-date').val();
     var index, curdate;
     var active_date=[];
+    var chosen_date=[];
     var dates = received_dates.split(",");
     var default_year, default_month,default_day;
     if (received_dates != ""){
@@ -22,7 +24,26 @@ $( document ).on('turbolinks:load',function()
 
             active_date.push(curdate[1] + "/" + curdate[0] + "/" + curdate[2]);
         }
-        //var active_dates = ["8/11/2017", "25/11/2017"];
+    if (chosen_dates != undefined) {
+            var chosendates = chosen_dates.split(",");
+            for (index = 0; index < chosendates.length; index++) {
+                curdate = chosendates[index].split("/");
+                if (index == 0) {
+                    default_year = curdate[2];
+                    default_month = curdate[0] - 1;
+                    default_day = curdate[1];
+                }
+                if (curdate[1][0] == '0') {
+                    curdate[1] = curdate[1].substr(1);
+                }
+                if (curdate[0][0] == '0') {
+                    curdate[0] = curdate[0].substr(1);
+                }
+
+                chosen_date.push(curdate[1] + "/" + curdate[0] + "/" + curdate[2]);
+            }
+        }
+            //var active_dates = ["8/11/2017", "25/11/2017"];
         $('#datepicker').datepicker(
             {
                 defaultViewDate: {year: default_year, month: default_month, day: default_day},
@@ -34,11 +55,17 @@ $( document ).on('turbolinks:load',function()
                     var curr_year = d.getFullYear();
                     var formattedDate = curr_date + "/" + curr_month + "/" + curr_year
 
-                    if ($.inArray(formattedDate, active_date) != -1) {
+                   if($.inArray(formattedDate, chosen_date) != -1){
+                        return{
+                            classes: 'active'
+                        };
+                    }
+                    else if ($.inArray(formattedDate, active_date) != -1) {
                         return {
                             classes: 'availableDate'
                         };
                     }
+
                     else {
                         return false
                     }
